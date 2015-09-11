b5eb68e12ca8b991d38c82786e0b9885a605c8e4
zakharovas <alexkru2011@gmail.com>
2015-09-11 20:29:58 +0300

    First Task added all whitespace characters, but it save "\".

--- src/ru/fizteh/fivt/students/zakharovas/reverser/Reverser.java
+++ src/ru/fizteh/fivt/students/zakharovas/reverser/Reverser.java
@@ -6,7 +6,7 @@ public class Reverser {
         for (int i = args.length - 1; i >= 0; --i) {
             String[] separatedArguments = args[i].split("\\s+");
             for (int j = separatedArguments.length - 1; j >= 0; --j) {
-                System.out.println(separatedArguments[j] + " ");
+                System.out.print(separatedArguments[j] + " ");
             }
         }
     }
