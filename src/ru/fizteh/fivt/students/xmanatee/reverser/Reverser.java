f62c01d4f34907af603ab0d98a2b9d9d6a99d3df
Michael Nemilov <michael666-96@mail.ru>
2015-09-13 02:05:01 +0300

    Task-0. Reverser. Nemilov Michael
    The first task is done

---
+++ src/ru/fizteh/fivt/students/xmanatee/reverser/Reverser.java
@@ -0,0 +1,29 @@
+package ru.fizteh.fivt.students.xmanatee.reverser;
+
+/**
+ * Class Reverser for reversing arguments of the command line.
+ */
+
+final class Reverser {
+    /**
+     * also for checkstyle...
+     */
+    private Reverser() {
+        // Prevent instantiation
+        // Optional: throw an exception e.g. AssertionError
+        // if this ever *is* called
+    }
+    /**
+     * Method main.
+     * @param args **arguments in the command line**
+     */
+    public static void main(final String... args) {
+        for (int i = args.length - 1; i >= 0; i--) {
+            String[] tokens = args[i].split("\\W");
+            for (int j = tokens.length - 1; j >= 0; j--) {
+                System.out.print(tokens[j] + " ");
+            }
+        }
+    }
+
+}
