2e38f1f4a9a123166ed5b3b35ebdcd8a69cf5415
riv <roller145@yandex.ru>
2015-09-14 01:02:25 +0300

    the working code of the right task

--- src/ru/fizteh/fivt/students/RudenkoIrina/1task/ReverseArgumentPrinter.java
+++ src/ru/fizteh/fivt/students/RudenkoIrina/1task/ReverseArgumentPrinter.java
@@ -1,8 +1,22 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
 public class ReverseArgumentPrinter {
-    public static void main(String [] args) {
-        for (int i = args.length - 1 ;  i>= 0; i--) {
-            String arg = args[i];
-            System.out.println(i + ": " + arg);
+    public static void main(String[] args) {
+        ArrayList <String> res = new ArrayList<>();
+        for (int i = 0; i < args.length; ++i ){
+            StringTokenizer splited = new StringTokenizer(args[i], " \\\t\"\n\r\f");
+            while(splited.hasMoreTokens()){
+                res.add(splited.nextToken());
+            }
+        }
+        for (int i = res.size() - 1; i >= 0; i-- ){
+
+            if (res.get(i).matches("\\d+")) {
+                System.out.print(res.get(i) + ' ');
+            }
         }
     }
 }
+
