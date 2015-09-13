d151b46b8626cfe3a5affb2a658e0a8713f38eb2
egiby <egiby>
2015-09-14 00:54:41 +0300

    First solution

--- src/ru/fizteh/fivt/students/AUpshinskiy/Reverser/Reverser.java
+++ src/ru/fizteh/fivt/students/AUpshinskiy/Reverser/Reverser.java
@@ -4,4 +4,25 @@ package ru.fizteh.fivt.students.AUpshinskiy.Reverser;
  * Created by egiby on 13.09.15.
  */
 public class Reverser {
+    /**
+     * @param args
+     * There is a main function.
+     */
+    public static void main(final String[] args) {
+
+        for (int i = args.length - 1; i >= 0; --i) {
+            String[] numbers = args[i].split("\\D");
+
+            for (int j = numbers.length - 1; j >= 0; --j) {
+                System.out.print(numbers[j] + " ");
+            }
+        }
+        System.out.print("\n");
+    }
+
+    /**
+     * There is a default constructor.
+     */
+    protected Reverser() {
+    }
 }
