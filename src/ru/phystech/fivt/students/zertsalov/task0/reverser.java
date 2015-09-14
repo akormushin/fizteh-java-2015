283fc723d3371bf581366dd2e155df38f4c94c71
Andrey <zertsalov96@yandex.ru>
2015-09-14 23:40:13 +0300

    reverser done

--- src/ru/phystech/fivt/students/zertsalov/task0/reverser.java
+++ src/ru/phystech/fivt/students/zertsalov/task0/reverser.java
@@ -4,6 +4,7 @@ package ru.phystech.fivt.students.zertsalov.task0;
  * task0 reverser
  * Created by User on 14.09.2015.
  */
+
 public class reverser {
     public static void main(String args[]){
         for (int i = args.length - 1; i >= 0; i--) {
