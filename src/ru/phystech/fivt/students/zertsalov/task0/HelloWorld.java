0b61834ee3307ead6323751bc551ca943e1e8cdb
Andrey <zertsalov96@yandex.ru>
2015-09-14 23:21:01 +0300

    hello world try

--- src/ru/phystech/fivt/students/zertsalov/task0/HelloWorld.java
+++ src/ru/phystech/fivt/students/zertsalov/task0/HelloWorld.java
@@ -1,14 +1,8 @@
 package ru.phystech.fivt.students.zertsalov.task0;
 
-/**
- * Created by User on 14.09.2015.
- */
 class HelloWorld {
-
-public static void main (String args []) {
-
-        System. out. println ("Hello, World");
-
+    public static void main(String args[]) {
+            System.out.println("Hello, World");
         }
 
 }
