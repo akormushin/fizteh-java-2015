201ab9fdf53b2453b17f453f92212649ff964323
veraklim <veraklim@mail.ru>
2015-09-14 23:59:15 +0300

    Reverser has been added

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Vera on 14.09.2015.
+ */
+public class Reverse {
+    public static void main(String[] args){
+        for(int i=args.length;i!=0;--i){
+           System.out.format("%d",args[i]);
+        }
+    }
+}
