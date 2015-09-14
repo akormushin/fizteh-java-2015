8316f66af92917fb9915b39724c19287472ef4eb
rtriangle <korbut.daniel@gmail.com>
2015-09-15 01:17:38 +0300

    second commit

--- src/ru/fizteh/fivt/students/rtriangle/reverser/Main.java
+++ src/ru/fizteh/fivt/students/rtriangle/reverser/Main.java
@@ -5,7 +5,7 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; --i) {
-            String[] number = args[i].split("[\\\\ ]");
+            String[] number = args[i].split("[\\s*\\\\*]+");
             for (int j = number.length - 1; j >= 0; j--) {
                 if (number[j] != " " && !number[j].equals("\\")) {
                     System.out.print(number[j] + ' ');
