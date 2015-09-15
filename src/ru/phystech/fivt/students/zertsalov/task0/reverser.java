a226fad3f8d0663290ad80f35aa61c1c17236da2
Andrey <zertsalov96@yandex.ru>
2015-09-15 16:37:27 +0300

    \\s+ fixed

--- src/ru/phystech/fivt/students/zertsalov/task0/reverser.java
+++ src/ru/phystech/fivt/students/zertsalov/task0/reverser.java
@@ -8,7 +8,7 @@ package ru.phystech.fivt.students.zertsalov.task0;
 public class reverser {
     public static void main(String args[]){
         for (int i = args.length - 1; i >= 0; i--) {
-            String[] curr = args[i].split("((\\s)|(/)|(\\\\))+");
+            String[] curr = args[i].split("\\s+");
             for (int j = curr.length - 1; j >= 0; j--){
                 System.out.print(curr[j] + " ");
             }
