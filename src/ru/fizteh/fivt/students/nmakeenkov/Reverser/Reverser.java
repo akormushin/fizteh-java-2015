ff484d49f357983e7023908199d0aa7300853231
Nikolay Makeenkov <rn3dko@yandex.ru>
2015-09-15 10:29:58 +0300

    Сhanged regex to "\s+"

--- src/ru/fizteh/fivt/students/nmakeenkov/Reverser/Reverser.java
+++ src/ru/fizteh/fivt/students/nmakeenkov/Reverser/Reverser.java
@@ -1,11 +1,9 @@
 package ru.fizteh.fivt.students.nmakeenkov.Reverser;
 
-import java.util.Arrays;
-
 public class Reverser {
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--) {
-            String[] curArg = args[i].split("[^\\d-]");
+            String[] curArg = args[i].split("\\s+");
             for (int j = curArg.length - 1; j >= 0; j--)
                 System.out.print(curArg[j] + " ");
         }
