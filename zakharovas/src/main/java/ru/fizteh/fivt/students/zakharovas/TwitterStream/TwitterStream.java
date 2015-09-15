61c3c8a83b3328603f04c8290fee2ffd289cac8e
zakharovas <alexkru2011@gmail.com>
2015-09-15 23:08:51 +0300

    Added new structure of project

---
+++ zakharovas/src/main/java/ru/fizteh/fivt/students/zakharovas/TwitterStream/TwitterStream.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.zakharovas.TwitterStream;
+
+public class TwitterStream {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
