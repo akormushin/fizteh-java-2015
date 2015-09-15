38c525ecb9f48baec72d4ce02482e5eda465de49
Andrey Putilin <regresscheck@gmail.com>
2015-09-15 09:03:46 +0300

    Fixed solution for 00-reverse

--- src/ru/fizteh/fivt/students/putilin/reverser/Reverser.java
+++ src/ru/fizteh/fivt/students/putilin/reverser/Reverser.java
@@ -3,7 +3,7 @@ package ru.fizteh.fivt.students.putilin.reverser;
 public class Reverser {
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--) {
-            String[] numbers = args[i].split("\\D");
+            String[] numbers = args[i].split("\\s+");
             for (int j = numbers.length - 1; j >= 0; j--)
                 System.out.print(numbers[j] + " ");
         }
