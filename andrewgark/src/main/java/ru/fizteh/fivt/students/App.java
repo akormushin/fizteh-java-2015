af6ef9d0aac2593c7f7fe65ac4ae91a3a21609f6
Andrey Garkavyy <andrewgarkavyy@gmail.com>
2015-09-21 21:19:46 +0300

    Style Update

--- andrewgark/src/main/java/ru/fizteh/fivt/students/App.java
+++ andrewgark/src/main/java/ru/fizteh/fivt/students/App.java
@@ -4,10 +4,8 @@ package ru.fizteh.fivt.students;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
