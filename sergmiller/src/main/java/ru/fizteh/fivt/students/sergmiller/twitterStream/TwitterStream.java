35a6f36486c730be236938e1819bbc264bbf18b1
sergmiller <sergey.miller@phystech.edu>
2015-09-21 03:35:41 +0300

    Empty TwitterStream project

---
+++ sergmiller/src/main/java/ru/fizteh/fivt/students/sergmiller/twitterStream/TwitterStream.java
@@ -0,0 +1,18 @@
+package ru.fizteh.fivt.students.sergmiller.twitterStream;
+
+/**
+ * Created by sergmiller on 21.09.15.
+ */
+final class TwitterStream {
+    /**
+     * also codestyle comment...
+     */
+    private TwitterStream() {
+    }
+    /**
+     * @param args is parametr of a search some new tweets
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello, Twitter");
+    }
+}
