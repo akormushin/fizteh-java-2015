cd67966dd4fb68b493814706dee2e81ef0da7347
Daniil Dmitriev <ocksumoron@yandex.ru>
2015-09-22 19:05:37 +0300

    whitespace 2

--- ocksumoron/src/main/java/ru/fizteh/fivt/students/ocksumoron/helloworld/App.java
+++ ocksumoron/src/main/java/ru/fizteh/fivt/students/ocksumoron/helloworld/App.java
@@ -4,10 +4,8 @@ package main.java.ru.fizteh.fivt.students.helloworld;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String[] args)
-    {
+public class App {
+    public static void main(String[] args) xw{
         System.out.println("Hello World!");
     }
 }
