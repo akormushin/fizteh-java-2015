5e283fc925cf1e727e4bea0801e3bdfff7663c97
Linkov Zhenya <evlinkov@gmail.com>
2015-09-23 14:15:32 +0300

    Linkov495

--- src/ru/fizteh/fivt/students/linkov/Reverser/Reverser.java
+++ src/ru/fizteh/fivt/students/linkov/Reverser/Reverser.java
@@ -4,11 +4,11 @@
 
 public class Reverser {
     public static void main(String[] args) {
-        String[] Numbers;
+        String[] numbers;
         for (int i = args.length - 1; i >= 0; i--) {
-            Numbers = args[i].split("\\s+");
-            for (int j = Numbers.length - 1; j >= 0; --j) {
-                System.out.print(Numbers[j] + " ");
+            numbers = args[i].split("\\s+");
+            for (int j = numbers.length - 1; j >= 0; --j) {
+                System.out.print(numbers[j] + " ");
             }
         }
         System.out.print("\n");
