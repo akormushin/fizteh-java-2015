1c6129497aca4ae52871df1dcebea7f8e4b54023
cache-nez <cache-nez@yandex.ru>
2015-09-28 01:16:35 +0300

    hideRetweets option and parameter validation added

--- cache-nez/src/main/java/ru/fizteh/fivt/students/cache_nez/TwitterStream/TweetsRetriever.java
+++ cache-nez/src/main/java/ru/fizteh/fivt/students/cache_nez/TwitterStream/TweetsRetriever.java
@@ -2,79 +2,164 @@ package ru.fizteh.fivt.students.cache_nez.TwitterStream;
 
 import twitter4j.*;
 
+import java.time.*;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAmount;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by cache-nez on 9/23/15.
  */
 
+class Declenser {
+    public enum ToDeclense { MINUTE, HOUR, DAY, RETWEET };
+    public static final int FIRST_TYPE = 1;
+    public static final int SECOND_TYPE_START = 2;
+    public static final int SECOND_TYPE_END = 4;
+    public static final int EXCEPTION_START = 11;
+    public static final int EXCEPTION_END = 14;
+    public static final int FIRST_MODULO = 100;
+    public static final int SECOND_MODULO = 10;
 
+    public static String getDeclension(long numeral, ToDeclense word) {
+        numeral %= FIRST_MODULO;
+        if (numeral >= EXCEPTION_START && numeral <= EXCEPTION_END) {
+            switch (word) {
+                case MINUTE: return " минут";
+                case HOUR: return " часов";
+                case DAY: return " дней";
+                case RETWEET: return "ретвитов";
+                default: assert true;
+            }
+        }
+        numeral %= SECOND_MODULO;
+        if (numeral == FIRST_TYPE) {
+            switch (word) {
+                case MINUTE: return " минуту";
+                case HOUR: return " час";
+                case DAY: return " день";
+                case RETWEET: return " ретвит";
+                default: assert true;
+            }
+        }
+        if (numeral >= SECOND_TYPE_START && numeral <= SECOND_TYPE_END) {
+            switch (word) {
+                case MINUTE: return " минуты";
+                case HOUR: return " часа";
+                case DAY:  return " дня";
+                case RETWEET: return " ретвита";
+                default: assert true;
+            }
+        }
+        switch (word) {
+            case MINUTE: return " минут";
+            case HOUR: return " часов";
+            case DAY: return " дней";
+            case RETWEET: return " ретвитов";
+            default: assert true;
+        }
+        assert true;
+        return  "";
+    }
+}
+
+class TimeConverter {
+
+    private static final TemporalAmount TWO_MINUTES = Duration.ofMinutes(2);
+    private static final TemporalAmount HOUR = Duration.ofHours(1);
+    private static final int MINUTES_IN_DAY = 24 * 60;
+    private static final int HOURS_IN_DAY = 60;
+
+
+    public static String getRelativeTime(Date date) {
+        LocalDateTime tweetDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime currentDate = LocalDateTime.now();
+        if (currentDate.minus(TWO_MINUTES).isBefore(tweetDate)) {
+            return "только что";
+        }
+        int delta;
+        if (currentDate.minus(HOUR).isBefore(tweetDate)) {
+            delta = currentDate.get(ChronoField.MINUTE_OF_DAY) - tweetDate.get(ChronoField.MINUTE_OF_DAY);
+            if (delta < 0) {
+                delta = MINUTES_IN_DAY + delta;
+            }
+            return delta + Declenser.getDeclension(delta, Declenser.ToDeclense.MINUTE) + " назад";
+        }
+        if (tweetDate.toLocalDate().equals(LocalDate.now())) {
+            delta = currentDate.get(ChronoField.HOUR_OF_DAY) - tweetDate.get(ChronoField.HOUR_OF_DAY);
+            if (delta < 0) {
+                delta = HOURS_IN_DAY + delta;
+            }
+            return delta + Declenser.getDeclension(delta, Declenser.ToDeclense.HOUR) + " назад";
+        }
+        if (tweetDate.toLocalDate().equals(LocalDate.now().minusDays(1))) {
+            return "вчера";
+        }
+        long days = currentDate.getLong(ChronoField.EPOCH_DAY) - tweetDate.getLong(ChronoField.EPOCH_DAY);
+        return  days + Declenser.getDeclension(days, Declenser.ToDeclense.DAY) + " назад";
+    }
+}
 
 
 public class TweetsRetriever {
-    private static final int DEFAULT_LIMIT = 10;
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_BLUE = "\u001B[34m";
 
-    static String getRelativeTime(Date date) {
-        /*TODO: set relative time*/
-        return date.toString();
-    }
-
-    static String getRetweetText (Status status) {
+    static String getRetweetText(Status status) {
+        /*remove prefix "<person> retweeted <person>"*/
         String[] splitText = status.getText().split(":", 2);
-        String tweetText = splitText[1];
-        return new StringBuilder().append("[").append(status.getCreatedAt().toString()).append("] @").append(ANSI_BLUE).append(status.getUser().getScreenName()).append(ANSI_RESET).append(" ретвитнул @").append(ANSI_BLUE).append(status.getRetweetedStatus().getUser().getScreenName()).append(ANSI_RESET).append(": ").append(tweetText).toString();
+
+        String tweetText = "[" + TimeConverter.getRelativeTime(status.getCreatedAt()) + "] ";
+        tweetText = tweetText + ANSI_BLUE + "@" + status.getUser().getScreenName() + ANSI_RESET;
+        tweetText = tweetText + " ретвитнул " + ANSI_BLUE + "@" + status.getRetweetedStatus().getUser().getScreenName()
+                    + ANSI_RESET;
+        tweetText = tweetText + ": " + splitText[1];
+        return tweetText;
     }
 
-    static String getTweetText (Status status) {
-        String text = "[" + status.getCreatedAt().toString() + "] @" + ANSI_BLUE + status.getUser().getScreenName()
-                + ANSI_RESET + ": " + status.getText();
+    static String getTweetText(Status status) {
+        String text = "[" + TimeConverter.getRelativeTime(status.getCreatedAt()) + "] ";
+        text = text + ANSI_BLUE + "@" + status.getUser().getScreenName() + ANSI_RESET;
+        text = text + ": " + status.getText();
         if (status.getRetweetCount() > 0) {
-            text = text + " (" + status.getRetweetCount() + " ретвитов)";
+            int retweets = status.getRetweetCount();
+            text = text + " (" + retweets + Declenser.getDeclension(retweets, Declenser.ToDeclense.RETWEET) + ")";
         }
         return  text;
     }
 
-    static String getTextToPrint(Status status) {
-        if (status.isRetweet()) {
-            return getRetweetText(status)
-        }
-        else {
-            return  getTweetText(status);
-        }
-    }
-
-    public static void getTweets(String searchFor, int limit) throws TwitterException {
+    public static void getTweets(String searchFor, int limit, boolean hideRetweets) throws TwitterException {
         Twitter twitter = new TwitterFactory().getInstance();
-        Query query;
-        if (searchFor.equals("")) {
-            query = new Query();
-        } else {
-            query = new Query(searchFor);
-        }
-        //query.setLang("ru");
+        Query query = new Query(searchFor);
         query.setCount(limit);
-        QueryResult result = null;
-        try {
-            result = twitter.search(query);
-        } catch (TwitterException e) {
-            //e.printStackTrace();
-            throw e;
-        }
-        if (result != null) {
-            for (Status status : result.getTweets()) {
-                System.out.println(getTextToPrint(status));
+        QueryResult result;
+        List<Status> tweets;
+        do {
+            try {
+                result = twitter.search(query);
+            } catch (TwitterException e) {
+                throw e;
             }
-        } else {
-            System.out.println("No tweets found");
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-
+            tweets = result.getTweets();
+            if (tweets.size() > 0) {
+                for (Status status : tweets) {
+                    if (status.isRetweet()) {
+                        if (!hideRetweets) {
+                            System.out.println(getRetweetText(status));
+                            --limit;
+                        }
+                    } else {
+                        System.out.println(getTweetText(status));
+                        --limit;
+                    }
+                }
+            } else {
+                System.out.println("No tweets found");
+                return;
+            }
+            query = result.nextQuery();
+        } while (limit > 0 && query != null);
     }
 }
 
--- cache-nez/src/main/java/ru/fizteh/fivt/students/cache_nez/TwitterStream/TwitterStream.java
+++ cache-nez/src/main/java/ru/fizteh/fivt/students/cache_nez/TwitterStream/TwitterStream.java
@@ -4,57 +4,37 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import twitter4j.TwitterException;
 
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by cache-nez on 9/27/15.
  */
-public class TwitterStream {
-    private static final int DEFAULT_LIMIT = 10;
 
-    @Parameter(names = {"--query", "-q"}, description = "Set keywords to find", required = true)
+
+
+public class TwitterStream {
+    private static final int DEFAULT_LIMIT = 100;
+
+    @Parameter(names = {"--query", "-q"}, description = "Set keywords to find.")
     private String query = "физтех";
 
-    @Parameter(names = {"--place", "-p"}, description = "Specify tweets location")
+    @Parameter(names = {"--place", "-p"}, description = "Specify tweets location.")
     private  String location;
 
-    @Parameter(names = {"--stream", "-s"}, description = "Determines if the stream mode is on: print a bunch of tweets every second")
+    @Parameter(names = {"--stream", "-s"},
+            description = "Determines if the stream mode is on: print a bunch of tweets every second. "
+                        + "Limit parameter is ignored.")
     private boolean streamMode = false;
 
-    @Parameter(names = "--hideRetweets", description = "Show original tweets only")
+    @Parameter(names = "--hideRetweets", description = "Show original tweets only.")
     private boolean hideRetweets = false;
 
-    @Parameter(names = {"--limit", "-l"}, description = "Specify a number of tweets to show")
+    @Parameter(names = {"--limit", "-l"}, description = "Specify a number of tweets to show.",
+            validateWith = IntegerValidator.class)
     private int tweetsLimit = DEFAULT_LIMIT;
 
-    @Parameter(names = {"--help", "-h"}, help = true, description = "Display help message")
+    @Parameter(names = {"--help", "-h"}, help = true, description = "Display help message.")
     private boolean showHelp = false;
 
-
-    public String getQuery() {
-        return query;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public boolean isStreamMode() {
-        return streamMode;
-    }
-
-    public boolean isHideRetweets() {
-        return hideRetweets;
-    }
-
-    public int getTweetsLimit() {
-        return tweetsLimit;
-    }
-
-    public boolean isShowHelp() {
-        return showHelp;
-    }
 
     public static void main(String[] args) {
         TwitterStream parseArgs = new TwitterStream();
@@ -62,20 +42,19 @@ public class TwitterStream {
         try {
             jCommander =  new JCommander(parseArgs, args);
         } catch (Exception wrongArgs) {
-            System.err.println("Wrong arguments");
+            System.err.println("Wrong arguments: " + wrongArgs.getMessage());
             return;
         }
-        if (parseArgs.isShowHelp()) {
+        if (parseArgs.showHelp) {
             jCommander.setProgramName("TwitterStream");
             jCommander.usage();
             return;
         }
 
         try {
-            TweetsRetriever.getTweets(parseArgs.getQuery(), parseArgs.getTweetsLimit());
+            TweetsRetriever.getTweets(parseArgs.query, parseArgs.tweetsLimit, parseArgs.hideRetweets);
         } catch (TwitterException e) {
             System.err.println("Error " + e.getErrorCode() + ": " +  e.getErrorMessage());
         }
-        /*TODO: */
     }
 }
