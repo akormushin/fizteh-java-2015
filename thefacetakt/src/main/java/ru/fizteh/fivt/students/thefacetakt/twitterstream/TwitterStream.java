91392223d4c493fa67892b502115dd5a24a7b281
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2015-09-27 21:17:02 +0300

    sphereDistance fixed

--- thefacetakt/src/main/java/ru/fizteh/fivt/students/thefacetakt/twitterstream/TwitterStream.java
+++ thefacetakt/src/main/java/ru/fizteh/fivt/students/thefacetakt/twitterstream/TwitterStream.java
@@ -156,8 +156,18 @@ public class TwitterStream {
 
     //https://en.wikipedia.org/wiki/Great-circle_distance#Formulas
     static final double EARTH_RADIUS = 6371;
+    static final double RADIANS_IN_DEGREE = Math.PI / 180;
+
+    static double toRadians(double angle) {
+        return angle * RADIANS_IN_DEGREE;
+    }
+
     static double sphereDistance(double phi1, double lambda1,
                                  double phi2, double lambda2) {
+        phi1 = toRadians(phi1);
+        phi2 = toRadians(phi2);
+        lambda1 = toRadians(lambda1);
+        lambda2 = toRadians(lambda2);
 
         double deltaPhi = phi2 - phi1;
         double deltaLambda = lambda2 - lambda1;
