c081fe5f914ebcf12d0c8cd996d327e6ead89fdd
egiby <egiby@egiby>
2015-09-29 02:17:11 +0300

    Something work

--- egiby/src/main/java/ru/fizteh/fivt/students/egiby/TwitterStream.java
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/TwitterStream.java
@@ -1,11 +1,52 @@
 package ru.fizteh.fivt.students.egiby;
 
+import twitter4j.*;
+import java.util.List;
+
 /**
  * Created by egiby on 24.09.15.
  */
 
 public class TwitterStream {
+    private static final Integer DEFAULT_NUMBER_TWEETS = 100;
+
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        if (args.length == 0) {
+            return;
+        }
+
+        if (args[0].equals("--query") || args[0].equals("-q")) {
+            try {
+                printAllTweets("Moscow");
+            } catch (TwitterException te) {
+                te.printStackTrace();
+            }
+        }
+    }
+
+    private static void printAllTweets(String keywords) throws TwitterException {
+        Twitter twitter = new TwitterFactory().getInstance();
+        Query query = new Query(keywords);
+        QueryResult result;
+
+        Integer numberOfTweets = 0;
+
+        while (numberOfTweets < DEFAULT_NUMBER_TWEETS && query != null) {
+            result = twitter.search(query);
+
+            List<Status> tweets = result.getTweets();
+            for (Status tweet : tweets) {
+                printTweet(tweet);
+                numberOfTweets++;
+            }
+
+            query = result.nextQuery();
+        }
+
+        System.exit(0);
+    }
+
+    private static void printTweet(Status tweet) {
+        System.out.println("@" + tweet.getUser().getScreenName() + " - " + tweet.getText());
     }
 }
