b8437bf58a0fe4e025856558f92dac5e42b296ed
egiby <egiby@egiby>
2015-09-28 22:27:43 +0300

    Initial commit

---
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/TwitterStream.java
@@ -0,0 +1,11 @@
+package ru.fizteh.fivt.students.egiby;
+
+/**
+ * Created by egiby on 24.09.15.
+ */
+
+public class TwitterStream {
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+}
