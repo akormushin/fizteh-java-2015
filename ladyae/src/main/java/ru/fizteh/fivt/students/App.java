3cabfdeea92926904109fe7c8b4f9b8ec7959018
Alyona Egorova <ladyae@ya.ru>
2015-09-28 12:15:00 +0300

    test

---
+++ ladyae/src/main/java/ru/fizteh/fivt/students/App.java
@@ -0,0 +1,13 @@
+package ru.fizteh.fivt.students;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
