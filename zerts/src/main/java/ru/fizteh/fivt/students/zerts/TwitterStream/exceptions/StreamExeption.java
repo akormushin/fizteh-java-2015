5ef6af6ed5db6548b9c2ef3314d8cc0abafa8cd4
Andrey <zertsalov96@yandex.ru>
2015-10-02 11:14:59 +0300

    Nearby feature upgraded, exeptions fixed, some minor fixes

--- zerts/src/main/java/ru/fizteh/fivt/students/zerts/TwitterStream/exceptions/StreamExeption.java
+++ zerts/src/main/java/ru/fizteh/fivt/students/zerts/TwitterStream/exceptions/StreamExeption.java
@@ -1,8 +1,5 @@
 package ru.fizteh.fivt.students.zerts.TwitterStream.exceptions;
 
-/**
- * Created by User on 02.10.2015.
- */
 public class StreamExeption extends Exception {
     public StreamExeption() {
     }
