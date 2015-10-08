1e64f1375e347cb35cdffbf5bd48b6c6b3782b97
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2015-10-09 00:09:26 +0300

    pom.xml fix

---
+++ thefacetakt/src/main/java/ru/fizteh/fivt/students/thefacetakt/twitterstream/exceptions/TwitterStreamException.java
@@ -0,0 +1,17 @@
+package ru.fizteh.fivt.students.thefacetakt.twitterstream.exceptions;
+
+/**
+ * Created by thefacetakt on 06.10.15.
+ */
+public class TwitterStreamException extends Exception {
+    public TwitterStreamException() {
+    }
+
+    public TwitterStreamException(String message) {
+        super(message);
+    }
+
+    public TwitterStreamException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
