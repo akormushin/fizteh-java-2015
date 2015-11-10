2e1aea525985a75b125e3d08e2c5517f10e06c9c
Александр Захаров <alexkru2011@gmail.com>
2015-11-10 22:27:54 +0300

    1 more exception test

--- zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
+++ zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
@@ -3,12 +3,13 @@ package ru.fizteh.fivt.students.zakharovas.twitterstream.library;
 import com.beust.jcommander.JCommander;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import ru.fizteh.fivt.students.zakharovas.twitterstream.CommandLineArgs;
-import twitter4j.GeoLocation;
-import twitter4j.Query;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
+import ru.fizteh.fivt.students.zakharovas.twitterstream.library.exceptions.EmptyResultException;
+import twitter4j.*;
+
+import java.util.ArrayList;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
@@ -21,13 +22,13 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class TwitterSearchModeTest {
 
+    @Mock Twitter twitter;
+
     @Test(expected = TwitterException.class)
     public void testTwitterExceptionThrowing() throws Exception {
 
         CommandLineArgs emptyArguments = new CommandLineArgs();
         new JCommander(emptyArguments, ArgumentSeparator.separateArguments(new String[]{"-q a"}));
-
-        Twitter twitter = mock(Twitter.class);
         TwitterException exception = mock(TwitterException.class);
         when(exception.isCausedByNetworkIssue()).thenReturn(false);
         when(twitter.search(any(Query.class))).thenThrow(exception);
@@ -37,6 +38,21 @@ public class TwitterSearchModeTest {
         when(emptyGeoLocator.getLocationForSearch()).thenReturn(location);
         when(emptyGeoLocator.getRadius()).thenReturn(0d);
 
+        TwitterSearchMode searchMode = new TwitterSearchMode(twitter, emptyArguments, emptyGeoLocator);
+        searchMode.search();
+    }
+
+    @Test(expected = EmptyResultException.class)
+    public void testOfEmptyResult() throws Exception {
+        CommandLineArgs emptyArguments = new CommandLineArgs();
+        new JCommander(emptyArguments, ArgumentSeparator.separateArguments(new String[]{"-q a"}));
+        QueryResult result = mock(QueryResult.class);
+        when(result.getTweets()).thenReturn(new ArrayList<Status>());
+        when(twitter.search(any(Query.class))).thenReturn(result);
+        GeoLocator emptyGeoLocator = mock(GeoLocator.class);
+        GeoLocation location = new GeoLocation(0, 0);
+        when(emptyGeoLocator.getLocationForSearch()).thenReturn(location);
+        when(emptyGeoLocator.getRadius()).thenReturn(0d);
         TwitterSearchMode searchMode = new TwitterSearchMode(twitter, emptyArguments, emptyGeoLocator);
         searchMode.search();
     }
