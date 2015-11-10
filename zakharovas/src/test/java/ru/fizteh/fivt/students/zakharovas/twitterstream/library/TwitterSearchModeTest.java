9ffe2dd16defc74c3113a56f946fa96a27222c66
Александр Захаров <alexkru2011@gmail.com>
2015-11-10 21:37:57 +0300

    Stupid test on catching exception

--- zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
+++ zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
@@ -10,8 +10,6 @@ import twitter4j.Query;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
-import java.util.ArrayList;
-
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -25,18 +23,20 @@ public class TwitterSearchModeTest {
 
     @Test(expected = TwitterException.class)
     public void testTwitterExceptionThrowing() throws Exception {
-        ArrayList<String> searchString = new ArrayList<>();
-        searchString.add("");
-        Twitter twitter = mock(Twitter.class);
-        TwitterException exception = mock(TwitterException.class);
+
         CommandLineArgs emptyArguments = new CommandLineArgs();
         new JCommander(emptyArguments, ArgumentSeparator.separateArguments(new String[]{"-q a"}));
-        GeoLocator emptyGeoLocator = mock(GeoLocator.class);
+
+        Twitter twitter = mock(Twitter.class);
+        TwitterException exception = mock(TwitterException.class);
         when(exception.isCausedByNetworkIssue()).thenReturn(false);
         when(twitter.search(any(Query.class))).thenThrow(exception);
+
+        GeoLocator emptyGeoLocator = mock(GeoLocator.class);
         GeoLocation location = new GeoLocation(0, 0);
         when(emptyGeoLocator.getLocationForSearch()).thenReturn(location);
         when(emptyGeoLocator.getRadius()).thenReturn(0d);
+
         TwitterSearchMode searchMode = new TwitterSearchMode(twitter, emptyArguments, emptyGeoLocator);
         searchMode.search();
     }
