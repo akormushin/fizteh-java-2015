d20b5858c02a55058b9bfd0a5f81ecb65eeadf7e
Александр Захаров <alexkru2011@gmail.com>
2015-11-10 21:36:22 +0300

    Stupid test on catching exception

--- zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
+++ zakharovas/src/test/java/ru/fizteh/fivt/students/zakharovas/twitterstream/library/TwitterSearchModeTest.java
@@ -1,8 +1,20 @@
 package ru.fizteh.fivt.students.zakharovas.twitterstream.library;
 
+import com.beust.jcommander.JCommander;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
+import ru.fizteh.fivt.students.zakharovas.twitterstream.CommandLineArgs;
+import twitter4j.GeoLocation;
+import twitter4j.Query;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+import java.util.ArrayList;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by alexander on 10.11.15.
@@ -10,9 +22,23 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TwitterSearchModeTest {
-    @Test
-    public void test() {
-        return;
+
+    @Test(expected = TwitterException.class)
+    public void testTwitterExceptionThrowing() throws Exception {
+        ArrayList<String> searchString = new ArrayList<>();
+        searchString.add("");
+        Twitter twitter = mock(Twitter.class);
+        TwitterException exception = mock(TwitterException.class);
+        CommandLineArgs emptyArguments = new CommandLineArgs();
+        new JCommander(emptyArguments, ArgumentSeparator.separateArguments(new String[]{"-q a"}));
+        GeoLocator emptyGeoLocator = mock(GeoLocator.class);
+        when(exception.isCausedByNetworkIssue()).thenReturn(false);
+        when(twitter.search(any(Query.class))).thenThrow(exception);
+        GeoLocation location = new GeoLocation(0, 0);
+        when(emptyGeoLocator.getLocationForSearch()).thenReturn(location);
+        when(emptyGeoLocator.getRadius()).thenReturn(0d);
+        TwitterSearchMode searchMode = new TwitterSearchMode(twitter, emptyArguments, emptyGeoLocator);
+        searchMode.search();
     }
 
 }
